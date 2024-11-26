12d0d58b147d089180fa851e70fefd81048e3ee4
SateeshBapanapalli <sateeshb1997@gmail.com>
2024-11-26 17:57:45 +0530

    re deploy

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const jsonServer = require("json-server");  // importing json-server library
+// const jsonServer = require("json-server");  // importing json-server library
 
 
-const server = jsonServer.create();
-const router = jsonServer.router("db.json")
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001;  // we can use any port number here
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json")
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 3001;  // we can use any port number here
 
-server.use(middlewares);
-server.use(router);
-server.listen(port)
+// server.use(middlewares);
+// server.use(router);
+// server.listen(port)
+
 
